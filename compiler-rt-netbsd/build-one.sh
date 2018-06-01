07159240f1363d3b47aa7641e19d7f7a7eb8e387
plusun <tomsun.0.7@gmail.com>
2018-06-01 04:00:11 +0000

    compiler-rt-netbsd: generalize build-one.sh

--- compiler-rt-netbsd/build-one.sh
+++ compiler-rt-netbsd/build-one.sh
@@ -2,9 +2,11 @@
 
 nr_threads=`getconf NPROCESSORS_CONF`
 
-export PATH=/usr/tooldir/bin/:$PATH
-export PATH=/usr/extern_tooldir/bin/:$PATH
-/usr/tooldir/bin/nbmake-amd64 -j$nr_threads $@ clean
-/usr/tooldir/bin/nbmake-amd64 -j$nr_threads $@ dependall
-/usr/tooldir/bin/nbmake-amd64 -j$nr_threads $@
-/usr/tooldir/bin/nbmake-amd64 -j$nr_threads $@ install
+netbsd_root=/public/
+
+export PATH=$netbsd_root/tooldir/bin/:$PATH
+export PATH=$netbsd_root/extern_tooldir/bin/:$PATH
+$netbsd_root/tooldir/bin/nbmake-amd64 -j$nr_threads $@ clean
+$netbsd_root/tooldir/bin/nbmake-amd64 -j$nr_threads $@ dependall
+$netbsd_root/tooldir/bin/nbmake-amd64 -j$nr_threads $@
+$netbsd_root/tooldir/bin/nbmake-amd64 -j$nr_threads $@ install
