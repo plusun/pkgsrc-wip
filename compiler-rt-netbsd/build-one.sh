8da6854a15e6ce8c00d5a131c1a70758d123c691
plusun <tomsun.0.7@gmail.com>
2018-06-02 07:32:43 +0000

    compiler-rt-netbsd: improve script for local compiling

--- compiler-rt-netbsd/build-one.sh
+++ compiler-rt-netbsd/build-one.sh
@@ -2,11 +2,21 @@
 
 nr_threads=`getconf NPROCESSORS_CONF`
 
-netbsd_root=/public/
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 work_dir target_dir"
+    exit 1
+fi
+
+set -e
+set -x
+
+netbsd_root=`readlink -f $1`
+target_dir=`readlink -f $2`
 
 export PATH=$netbsd_root/tooldir/bin/:$PATH
 export PATH=$netbsd_root/extern_tooldir/bin/:$PATH
-$netbsd_root/tooldir/bin/nbmake-amd64 -j$nr_threads $@ clean
-$netbsd_root/tooldir/bin/nbmake-amd64 -j$nr_threads $@ dependall
-$netbsd_root/tooldir/bin/nbmake-amd64 -j$nr_threads $@
-$netbsd_root/tooldir/bin/nbmake-amd64 -j$nr_threads $@ install
+(cd $target_dir; \
+ $netbsd_root/tooldir/bin/nbmake-amd64 -j$nr_threads $@ clean; \
+ $netbsd_root/tooldir/bin/nbmake-amd64 -j$nr_threads $@ dependall; \
+ $netbsd_root/tooldir/bin/nbmake-amd64 -j$nr_threads $@; \
+ $netbsd_root/tooldir/bin/nbmake-amd64 -j$nr_threads $@ install)
