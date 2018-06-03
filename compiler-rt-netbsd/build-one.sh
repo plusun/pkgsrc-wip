7283bd0398b8e91b4e46bb0b9e86e2ddb0911d2f
plusun <tomsun.0.7@gmail.com>
2018-06-03 08:06:45 +0000

    compiler-rt-netbsd: add target for make

--- compiler-rt-netbsd/build-one.sh
+++ compiler-rt-netbsd/build-one.sh
@@ -2,7 +2,7 @@
 
 nr_threads=`getconf NPROCESSORS_CONF`
 
-if [ "$#" -ne 2 ]; then
+if [ "$#" -lt 2 ]; then
     echo "Usage: $0 work_dir target_dir"
     exit 1
 fi
@@ -13,10 +13,19 @@ set -x
 netbsd_root=`readlink -f $1`
 target_dir=`readlink -f $2`
 
+count=0
+target=
+for i in $@; do
+    count=$(($count + 1))
+    if [ $count -ne 1 ] && [ $count -ne 2 ]; then
+	target=$target" "$i
+    fi
+done
+
 export PATH=$netbsd_root/tooldir/bin/:$PATH
 export PATH=$netbsd_root/extern_tooldir/bin/:$PATH
 (cd $target_dir; \
- $netbsd_root/tooldir/bin/nbmake-amd64 -j$nr_threads clean; \
- $netbsd_root/tooldir/bin/nbmake-amd64 -j$nr_threads dependall; \
- $netbsd_root/tooldir/bin/nbmake-amd64 -j$nr_threads ; \
- $netbsd_root/tooldir/bin/nbmake-amd64 -j$nr_threads install)
+ $netbsd_root/tooldir/bin/nbmake-amd64 -j$nr_threads $target clean; \
+ $netbsd_root/tooldir/bin/nbmake-amd64 -j$nr_threads $target dependall; \
+ $netbsd_root/tooldir/bin/nbmake-amd64 -j$nr_threads $target; \
+ $netbsd_root/tooldir/bin/nbmake-amd64 -j$nr_threads $target install)
