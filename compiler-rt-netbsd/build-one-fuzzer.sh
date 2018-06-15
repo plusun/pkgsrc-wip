d96cdb3b5ff373c737183b045efbb97d16314f0d
plusun <tomsun.0.7@gmail.com>
2018-06-15 01:56:00 +0000

    Add building script for single project with fuzzer

---
+++ compiler-rt-netbsd/build-one-fuzzer.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+nr_threads=`getconf NPROCESSORS_CONF`
+
+if [ "$#" -lt 3 ]; then
+    echo "Usage: $0 work_dir no_link sanitizers"
+    exit 1
+fi
+
+set -e
+set -x
+
+netbsd_root=`readlink -f $1`
+no_link=$2
+MAKE_FLAGS='DESTDIR='"$netbsd_root/destdir/"' USETOOLS=no MKLLVM=yes HAVE_LLVM=yes MKGCC=no'
+SANITIZERS=$3
+
+count=0
+target=
+for i in $@; do
+    count=$(($count + 1))
+    if [ $count -gt 3 ]; then
+	target=$target" "$i
+    fi
+done
+
+LLVM_BIN=$netbsd_root/llvm-build/bin/
+COMPILE_FLAGS='-DENABLE_FUZZER -fsanitize=fuzzer-no-link,'"$SANITIZERS"' -g -O0'
+LINK_FLAGS='-DENABLE_FUZZER -fsanitize=fuzzer,'"$SANITIZERS"' -g -O0'
+CCFLAGS='CC='"$LLVM_BIN"'/clang CFLAGS="'"$COMPILE_FLAGS"'" CXX='"$LLVM_BIN"'/clang++ CXXFLAGS="'"$COMPILE_FLAGS"'"'
+LDFLAGS='LDFLAGS="'"$LINK_FLAGS"'"'
+LIB_LDFLAGS='LDFLAGS="'"$COMPILE_FLAGS"'"'
+
+if [ "$no_link" = "true" ]; then
+    ALL_FLAGS="$MAKE_FLAGS $CCFLAGS $LIB_LDFLAGS"
+else
+    ALL_FLAGS="$MAKE_FLAGS $CCFLAGS $LDFLAGS"
+fi
+
+build="make $ALL_FLAGS $target"
+eval $build
